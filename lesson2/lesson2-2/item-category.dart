56236042d45fb23bc7bdd1aa9777c603961e7b64
eldar-05 <eldarzh123@gmail.com>
2025-09-30 09:17:19 +0600

    upt

--- lesson2/lesson2-2/item-category.dart
+++ lesson2/lesson2-2/item-category.dart
@@ -1,23 +1,28 @@
 
 void main() {
-  Item item1 = Item("Apple", "Fruit", 100, 10);
+  Item item1 = Item("Apple", "Fruit", 100, 25);
+  Item item2 = Item("Dell G15", "Laptop", 10000, 12.5);
   print(item1.getFinalPrice());
   print(item1.getFullName());
+  print(item2.getFinalPrice() + " - " +  item2.getFullName());
 }
 
 class Item{
   String name;
   String category;
   double price;
-  int discount;
+  double discount;
   Item (this.name, this.category, this.price, [this.discount = 0]);
 
-  double getFinalPrice(){
-    return price - (price * (discount / 100));
+  String getFinalPrice(){
+    double finalPrice = price;
+    finalPrice = price - (price * (discount / 100.0));
+    return '$finalPrice сом';
   }
                     
   String getFullName(){
     return '$category $name';
   }
-
 }
+
+
